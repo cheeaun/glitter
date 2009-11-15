ca1e34bf503c1191a43c077d08b4a6b4c4bfc3a8
Lim Chee Aun <cheeaun@gmail.com>
2009-11-15 20:48:39 +0800

    External links load in browsers instead of inside Glitter.

--- Resources/userscripts/glitter.js
+++ Resources/userscripts/glitter.js
@@ -20,78 +20,96 @@
 	document.body.appendChild(jq);
 	
 	jq.onload = function(){
+		console.log('onload');
+		
 		if (!window.jQuery) return;
 		
 		jQuery.noConflict();
 		
 		var $ = jQuery;
 		
-		// Only run once, by detecting if this iframe exist in the body
-		// else this code will run inside every iframe
 		var $canvasFrame = $('#canvas_frame');
-		if (!$canvasFrame.length){
-			delete jQuery;
-			return;
-		}
-		
-		var unreadMsgCount = -1;
-		var mails = [];
-		
-		var options = {
-			initialDelay: 5,
-			pollInterval: 30,
-			priority: 5,
-			sticky: false,
-			trimLength: 150
-		};
-		
-		var selector = {
-			inbox: 'a.n0[title^=Inbox]',
-			unreadMessage: 'tr.zA.zE',
-			sender: 'span.zF',
-			subject: '.y6 span:first',
-			body: '.y2'
-		};
-		
-		var trimText = function(str){
-			return $.trim(str).substring(0, options.trimLength);
-		};
-		
-		var growlMsg = function(){
-			var oldCount = unreadMsgCount;
-			console.log('oldCount: ' + oldCount);
+		if ($canvasFrame.length){
 			
-			if (!mails.length) mails = $canvasFrame.contents();
-			var matches = mails.find(selector.inbox).text().match(/\((\d*)\)/);
-			if (matches) console.log('matches[1]: ' + matches[1]);
-			unreadMsgCount = (matches) ? matches[1] : 0;
-			console.log('unreadMsgCount: ' + unreadMsgCount);
-			
-			if (oldCount == -1){
-				console.log('oldCount == -1');
-				var title = unreadMsgCount + ' new message' + (unreadMsgCount == 0 || unreadMsgCount > 1 ? 's' : '');
-				var note = Titanium.Notification.createNotification(window);
-				note.setTitle(title);
-				note.setMessage('');
-				note.show();
-			} else if (unreadMsgCount > oldCount){
-				console.log('unreadMsgCount > oldCount');
-				var currentMsg = 0;
-				mails.find(selector.unreadMessage).each(function(){
-					if (++currentMsg > (unreadMsgCount - oldCount)) return false;
-					var el = $(this);
-					var title = 'New Message from ' + trimText(el.find(selector.sender).text());
-					var msg = trimText(el.find(selector.subject).text()) + trimText(el.find(selector.body).text());
+			var unreadMsgCount = -1;
+			var mails = [];
+
+			var options = {
+				initialDelay: 5,
+				pollInterval: 30,
+				priority: 5,
+				sticky: false,
+				trimLength: 150
+			};
+
+			var selector = {
+				inbox: 'a.n0[title^=Inbox]',
+				unreadMessage: 'tr.zA.zE',
+				sender: 'span.zF',
+				subject: '.y6 span:first',
+				body: '.y2'
+			};
+
+			var trimText = function(str){
+				return $.trim(str).substring(0, options.trimLength);
+			};
+
+			var growlMsg = function(){
+				var oldCount = unreadMsgCount;
+				console.log('oldCount: ' + oldCount);
+
+				if (!mails.length) mails = $canvasFrame.contents();
+				var matches = mails.find(selector.inbox).text().match(/\((\d*)\)/);
+				if (matches) console.log('matches[1]: ' + matches[1]);
+				unreadMsgCount = (matches) ? matches[1] : 0;
+				console.log('unreadMsgCount: ' + unreadMsgCount);
+
+				if (oldCount == -1){
+					console.log('oldCount == -1');
+					var title = unreadMsgCount + ' new message' + (unreadMsgCount == 0 || unreadMsgCount > 1 ? 's' : '');
 					var note = Titanium.Notification.createNotification(window);
 					note.setTitle(title);
-					note.setMessage(msg);
+					note.setMessage('');
 					note.show();
+				} else if (unreadMsgCount > oldCount){
+					console.log('unreadMsgCount > oldCount');
+					var currentMsg = 0;
+					mails.find(selector.unreadMessage).each(function(){
+						if (++currentMsg > (unreadMsgCount - oldCount)) return false;
+						var el = $(this);
+						var title = 'New Message from ' + trimText(el.find(selector.sender).text());
+						var msg = trimText(el.find(selector.subject).text()) + trimText(el.find(selector.body).text());
+						var note = Titanium.Notification.createNotification(window);
+						note.setTitle(title);
+						note.setMessage(msg);
+						note.setCallback(function(){
+							var win = Titanium.UI.getCurrentWindow();
+							win.focus();
+						});
+						note.show();
+					});
+				}
+			};
+
+			setTimeout(growlMsg, options.initialDelay * 1000);
+			setInterval(growlMsg, options.pollInterval * 1000);
+
+			setInterval(function(){
+				var extLinks = mails.find('a[target=_blank]');
+				if (!extLinks.length) return;
+				extLinks.attr('target', 'ti:systembrowser').click(function(){
+					Titanium.Desktop.openURL(this.href);
+					return false;
 				});
-			}
-		};
-		
-		setTimeout(growlMsg, options.initialDelay * 1000);
-		setInterval(growlMsg, options.pollInterval * 1000);
+			}, 1000);
+			
+		} else {
+			
+			delete jQuery;
+			delete Titanium;
+			return;
+			
+		}
 	};
 	
 })();
